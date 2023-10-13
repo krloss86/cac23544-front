92b2367f932508ba7a0b4735fd971cbe44982094
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-10-12 21:59:24 -0300

    array + funciones + dom + ajercicio

---
+++ assets/js/dom-functions.js
@@ -0,0 +1,16 @@
+
+function getValueById(id) {
+    return document.getElementById(id).value;
+}
+function setFocusById(id) {//MI FUNCION
+    //via D.O.M
+    document.getElementById(id).focus();
+}
+function updateValueById(id,value) {
+    //actualizar por medio del D.O.M el html
+    document.getElementById(id).value = value;
+}
+function updateHtmlById(id,value) {
+    //actualizar por medio del D.O.M el html
+    document.getElementById(id).innerHTML = value;
+}
