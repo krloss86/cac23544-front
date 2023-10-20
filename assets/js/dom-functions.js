43057719e7ec7fdad354028a3996d95e5a14b0a9
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-10-19 22:02:25 -0300

    fech api + ejemplo reqres

--- assets/js/dom-functions.js
+++ assets/js/dom-functions.js
@@ -2,6 +2,9 @@
 function getValueById(id) {
     return document.getElementById(id).value;
 }
+function getById(id) {
+    return document.getElementById(id);
+}
 function setFocusById(id) {//MI FUNCION
     //via D.O.M
     document.getElementById(id).focus();
